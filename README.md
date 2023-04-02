0781971fdfbb0a5a7f11f41cf3ac691d3a59af41
g93sambo <adetunlesearowolo@gmail.com>
2023-04-02 09:44:41 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Little-Lemon Restaurant
+
+## Capstone project for Meta front-end developer professional certificate.
+
+### **To run and use this project do the following steps.**
+
+- Download or clone the repository.
+- In the terminal run "npm install" or "npm audit fix --force"
+- After the node_modules have been installed run "npm start"
+
+#### _This project is built on ReactJS and React styled component_
+
+<p>
+<img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Meta-Logo.png" width="250"/>
+<img src="https://1000logos.net/wp-content/uploads/2022/06/Coursera-Logo.png" width="250" />
+</p>
+
+
+https://user-images.githubusercontent.com/64060848/212680743-c8e3c8e3-400e-4196-8b8d-719ad7a2b970.MOV
---
+++ src/components/Products/data.js
@@ -0,0 +1,66 @@
+import product1 from '../../images/product-1.jpg';
+import product2 from '../../images/product-2.jpg';
+import product3 from '../../images/product-3.jpg';
+import sweet1 from '../../images/sweet3.jpg';
+import sweet2 from '../../images/sweet-2.jpg';
+import sweet3 from '../../images/sweet-3.jpg';
+
+export const productData = [
+  {
+    img: product1,
+    id: 1,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc: 'Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$19.99',
+    button: 'Add to Cart',
+  },
+  {
+    img: product2,
+    id: 2,
+    alt: 'Pizza',
+    name: 'Hawaiian Paradise',
+    desc: ' Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$16.99',
+    button: 'Add to Cart',
+  },
+  {
+    img: product3,
+    id: 3,
+    alt: 'Pizza',
+    name: 'Veggie Overload',
+    desc: ' Marinara sauce, basil, italian sausage, roma tomatoes, olives, and pesto',
+    price: '$14.99',
+    button: 'Add to Cart',
+  },
+];
+
+export const productDataTwo = [
+  {
+    img: sweet2,
+    id: 4,
+    alt: 'Donuts',
+    name: 'Doughlicious',
+    desc: 'Belgian chocolate glazed donuts covered in icing with sprinkles on top',
+    price: '$9.99',
+    button: 'Add to Cart',
+  },
+  {
+    img: sweet3,
+    id: 5,
+    alt: 'Ice Cream',
+    name: 'Caramel Wonder',
+    desc: 'Vanilla ice cream covered with caramel and chocolate glaze topped with a coco stick',
+    price: '$12.99',
+    button: 'Add to Cart',
+  },
+  {
+    img: sweet1,
+    id: 6,
+    alt: 'Brownie',
+    name: 'Brownie Bunch',
+    desc: 'Double fudge brownie squares topped with white chocolate pieces and macadamia nuts',
+    price: '$9.99',
+    button: 'Add to Cart',
+  },
+];
